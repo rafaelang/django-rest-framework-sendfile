a5081d3c564373d156990196ee6cc153bbd3445b
Rafael Garcia <rgarcia@raro.com.br>
2015-09-03 14:14:27 -0300

    initial

---
+++ views.py
@@ -0,0 +1,30 @@
+from rest_framework.decorators import detail_route, parser_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+
+
+class UpdateUploadMixin(object):
+    upload_fields = []
+
+    @detail_route(methods=['POST'], permission_classes=[IsAuthenticated])
+    @parser_classes((FormParser, MultiPartParser,))
+    def file(self, request, pk):
+        for upload in self.upload_fields:
+            if upload in request.FILES:
+                instance = self.get_object()
+                instance.__dict__[upload].delete()
+
+                upload_data = request.data[upload]
+
+                instance.__dict__[upload].save(upload_data.name, upload_data)
+
+                return Response(instance.__dict__[upload], status=HTTP_201_CREATED)
+            else:
+                return Response(status=HTTP_400_BAD_REQUEST)
+
+    @detail_route(methods=['POST'], permission_classes=[IsAuthenticated])
+    @parser_classes((FormParser, MultiPartParser,))
+    def image(self, request, pk):
+        return self.file(request, pk)
