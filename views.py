110f716d7eefdd8c3aeaac1617f477010ab62c6f
Rafael Garcia <rgarcia@raro.com.br>
2015-09-04 15:12:18 -0300

    add getattr to ensure complete load object

--- views.py
+++ views.py
@@ -8,12 +8,18 @@ from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 class UpdateUploadMixin(object):
     upload_fields = []
 
+    def __init__(self, *args, **kwargs):
+        super(UpdateUploadMixin, self).__init__(*args, **kwargs)
+
     @detail_route(methods=['POST'], permission_classes=[IsAuthenticated])
     @parser_classes((FormParser, MultiPartParser,))
     def file(self, request, pk):
         for upload in self.upload_fields:
             if upload in request.FILES:
                 instance = self.get_object()
+
+                getattr(instance, upload)
+
                 instance.__dict__[upload].delete()
 
                 upload_data = request.data[upload]
@@ -23,6 +29,7 @@ class UpdateUploadMixin(object):
                 return Response(instance.__dict__[upload], status=HTTP_201_CREATED)
             else:
                 return Response(status=HTTP_400_BAD_REQUEST)
+        return Response(status=HTTP_400_BAD_REQUEST)
 
     @detail_route(methods=['POST'], permission_classes=[IsAuthenticated])
     @parser_classes((FormParser, MultiPartParser,))
